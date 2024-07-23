62f4d0e640c22ca413be31dacff2a8d841e23d86
Gwenaël Hautrive <awtomobilcar@gmail.com>
2024-07-23 11:20:26 +0200

    Initial Commit

---
+++ Drive 2 VTC/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Drive 2 VTC
+//
+//  Created by Gwenael Hautrive on 23/07/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Drive 2 VTC/Drive_2_VTCApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drive_2_VTCApp.swift
+//  Drive 2 VTC
+//
+//  Created by Gwenael Hautrive on 23/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct Drive_2_VTCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
